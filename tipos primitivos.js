b2aedf9a1418ce6c560685bd24a30e10903bd048
luandiniz-ssk <107211389+luandiniz-ssk@users.noreply.github.com>
2025-10-25 22:32:48 -0300

    Add files via upload

---
+++ tipos primitivos.js
@@ -0,0 +1,16 @@
+const nome = 'Luan'; // string
+const num1= 45;  // number
+const num2 = 10.10;
+let NomeAluno //undefined -> não apontam para lugar nenhum na memória
+let NomeGata = null; //nulo -> não apontam para lugar nenhum na memória
+// const boolean = True and False; exemplo
+const aprovado = true;
+const NaoAproved = false;
+console.log(aprovado);
+console.log(NaoAproved);
+
+console.log(typeof num1,num2,aprovado);
+
+
+
+
