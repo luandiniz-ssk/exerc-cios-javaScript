0aba54f5a1cf5f213c8140f44661e82902885ee8
luandiniz-ssk <107211389+luandiniz-ssk@users.noreply.github.com>
2025-10-25 22:35:53 -0300

    Update exercicios.js

--- exercicios.js
+++ exercicios.js
@@ -1,37 +1,37 @@
-// const nome = 'Luan';
-// const sobrenome = "Diniz";
-// const idade = 21;
-// const peso = 70;
-// const altura = 1.68;
-// let imc;  //peso / (altura + altura)
-// let AnoNascimento;
+const nome = 'Luan';
+const sobrenome = "Diniz";
+const idade = 21;
+const peso = 70;
+const altura = 1.68;
+let imc;  //peso / (altura + altura)
+let AnoNascimento;
 
-// imc = peso / (altura + altura);
-// // console.log(imc)
-// // AnoNascimento = 2025 - idade;
-// // console.log(AnoNascimento);
+imc = peso / (altura + altura);
+// console.log(imc)
+// AnoNascimento = 2025 - idade;
+// console.log(AnoNascimento);
 
-// // // Vamos ter três exemplos abaixo para usar nos codigos, todos estão de forma correta
-// // console.log(nome + ' ' + sobrenome + ' tem ' + idade + ' ano, pesa ' + peso + ' kilos ');
-// // console.log(`tem ${altura} de altura e seu imc é ${imc}`);
-// // console.log(nome, 'Naceu em', AnoNascimento, '.');
+// // / Vamos ter três exemplos abaixo para usar nos codigos, todos estão de forma correta
+console.log(nome + ' ' + sobrenome + ' tem ' + idade + ' ano, pesa ' + peso + ' kilos ');
+console.log(`tem ${altura} de altura e seu imc é ${imc}`);
+console.log(nome, 'Naceu em', AnoNascimento, '.');
 
 
-// const nome = "Luan";
-// const sobrenome = "Diniz";
-// const idade = 21;
-// const peso = 70;
-// let altura = 1.70;
-// let imc = peso / (altura + altura);
-// let AnoNascimento = 2025 - idade;
+const nome = "Luan";
+const sobrenome = "Diniz";
+const idade = 21;
+const peso = 70;
+let altura = 1.70;
+let imc = peso / (altura + altura);
+let AnoNascimento = 2025 - idade;
 
-// console.log(nome + ' ' + sobrenome + ' Tem ' + idade + ' anos  de idade ');
-// console.log(' Seu imc é ' + imc );
-// console.log(nome + ' Nasceu em ' + AnoNascimento);
+console.log(nome + ' ' + sobrenome + ' Tem ' + idade + ' anos  de idade ');
+console.log(' Seu imc é ' + imc );
+console.log(nome + ' Nasceu em ' + AnoNascimento);
 
-// console.log(`${nome} tem ${idade} anos de idade pesa ${peso} e seu imc é ${imc}`);
+console.log(`${nome} tem ${idade} anos de idade pesa ${peso} e seu imc é ${imc}`);
 
-// console.log(nome,"Nasceu em",AnoNascimento,"e seu imc é",imc);
+console.log(nome,"Nasceu em",AnoNascimento,"e seu imc é",imc);
 
 
 
@@ -48,3 +48,4 @@ console.log(peso + ' de peso ' + ' e seu imc é ' + imc);
 
 console.log(`${nome} tem ${idade} de idade, tem ${peso} de peso`);
 console.log(`tem ${altura} de altura e seu imc é de ${imc}`);
+
